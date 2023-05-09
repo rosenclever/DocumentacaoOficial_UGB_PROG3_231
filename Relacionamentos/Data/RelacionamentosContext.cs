d9c339e4a586996376c596519e5c62efa191d66e
rosenclever <rosenclever@gmail.com>
2023-05-09 20:48:05 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamentos/Data/RelacionamentosContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relacionamentos.Models;
+
+namespace Relacionamentos.Data
+{
+    public class RelacionamentosContext : DbContext
+    {
+        public RelacionamentosContext(DbContextOptions<RelacionamentosContext> options) : base(options)
+        {
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Relacionamentos/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Relacionamentos.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual Uri SiteUri { get; set; }
+
+        public ICollection<Post> Posts { get; }
+    }
+}
