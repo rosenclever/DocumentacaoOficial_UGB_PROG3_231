e81983d9e140c476aac44f680e1b6fbabd56a55d
rosenclever <rosenclever@gmail.com>
2023-05-16 20:29:57 -0300

    scaffolding de blog e post funcionando

--- Relacionamentos/Models/Blog.cs
+++ Relacionamentos/Models/Blog.cs
@@ -4,10 +4,10 @@ namespace Relacionamentos.Models
 {
     public class Blog
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public virtual Uri SiteUri { get; set; }
 
-        public ICollection<Post> Posts { get; }
+        public ICollection<Post>? Posts { get; }
     }
 }
--- Relacionamentos/Models/Post.cs
+++ Relacionamentos/Models/Post.cs
@@ -2,13 +2,13 @@
 {
     public class Post
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime PublishedOn { get; set; }
         public bool Archived { get; set; }
 
         public int BlogId { get; set; }
-        public Blog Blog { get; set; }
+        public Blog? Blog { get; set; }
     }
 }
